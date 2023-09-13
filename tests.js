5ddc8c373997a052518d300d529dbf7932f260ba
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-09-13 15:27:38 -0500

    add the first test and solution for intro-to-testing

---
+++ tests.js
@@ -0,0 +1,32 @@
+// unit tests for sayHello
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+});
+
+// Unit tests for the helloWorld function
+describe('helloWorld', function() {
+    it('should be a defined function', function() {
+      expect(typeof helloWorld).toBe('function');
+    });
+    it('should return a string when called', function() {
+      expect(typeof helloWorld()).toBe("string");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+      expect(helloWorld()).toBe("Hello, World!");
+    });
+    it("should never return 'undefined' when called", function() {
+      expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
